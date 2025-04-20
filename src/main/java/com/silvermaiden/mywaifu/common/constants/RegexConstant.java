2bc901f7185eb51f8327c77a76069231f92f85c2
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-04-21 00:18:40 +0700

    feat: add RegexConstant

---
+++ src/main/java/com/silvermaiden/mywaifu/common/constants/RegexConstant.java
@@ -0,0 +1,5 @@
+package com.silvermaiden.mywaifu.common.constants;
+
+public class RegexConstant {
+    public static final String REGEX_NO_BOOLEAN_AND_NUMBER = "^(?!\\d+$|true$|false$)[a-zA-Z][a-zA-Z0-9_]*$";
+}
