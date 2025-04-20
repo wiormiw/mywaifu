e6dd9de30aa76801eaba95a7fe815f97e82a1888
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-04-20 21:35:57 +0700

    chore: remove unused import

--- src/main/java/com/silvermaiden/mywaifu/configurations/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/silvermaiden/mywaifu/configurations/exceptions/GlobalExceptionHandler.java
@@ -7,13 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.silvermaiden.mywaifu.common.constants.ErrorCode.*;
 import static com.silvermaiden.mywaifu.common.constants.HTTPConstant.DEFAULT_FAILED_MESSAGE;
