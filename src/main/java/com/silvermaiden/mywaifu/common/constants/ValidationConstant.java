88d1a4644b9ac40f6121e90d3ce58810d703a7ff
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-04-21 00:19:29 +0700

    feat: add no number + boolean validation

--- src/main/java/com/silvermaiden/mywaifu/common/constants/ValidationConstant.java
+++ src/main/java/com/silvermaiden/mywaifu/common/constants/ValidationConstant.java
@@ -1,8 +1,12 @@
 package com.silvermaiden.mywaifu.common.constants;
 
 public class ValidationConstant {
+    //Default
+    public static final String DEFAULT_VALIDATION_ERROR_MESSAGE = "Validation error!";
+
     //Common
     public static final String FIELD_REQUIRED_MESSAGE = "Need to provide field, ";
+    public static final String NO_NUMBER_AND_BOOLEAN = "No number and boolean value for field, ";
     // Auth
     public static final String LOGIN_VALIDATION_MESSAGE = "Need username/password to login!";
     public static final String NO_DEFAULT_ROLE_MESSAGE = "Cannot create user with default role, contact admin!";
--- src/main/java/com/silvermaiden/mywaifu/models/dtos/user/UserRequestDTO.java
+++ src/main/java/com/silvermaiden/mywaifu/models/dtos/user/UserRequestDTO.java
@@ -3,20 +3,25 @@ package com.silvermaiden.mywaifu.models.dtos.user;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import static com.silvermaiden.mywaifu.common.constants.RegexConstant.REGEX_NO_BOOLEAN_AND_NUMBER;
 import static com.silvermaiden.mywaifu.common.constants.ValidationConstant.*;
 
-public record UserCreateDTO(
+public record UserRequestDTO(
         @NotBlank(message = FIELD_REQUIRED_MESSAGE + "username")
+        @Pattern(regexp = REGEX_NO_BOOLEAN_AND_NUMBER, message = NO_NUMBER_AND_BOOLEAN + "username")
         String username,
         @NotBlank(message = FIELD_REQUIRED_MESSAGE + "name")
+        @Pattern(regexp = REGEX_NO_BOOLEAN_AND_NUMBER, message = NO_NUMBER_AND_BOOLEAN + "name")
         String name,
         @NotBlank(message = FIELD_REQUIRED_MESSAGE + "password")
+        @Pattern(regexp = REGEX_NO_BOOLEAN_AND_NUMBER, message = NO_NUMBER_AND_BOOLEAN + "password")
         @Pattern(
                 regexp = "^(?=.*[A-Z])[A-Za-z0-9]+$",
                 message = INVALID_PASSWORD_MESSAGE
         )
         String password,
         @NotBlank(message = FIELD_REQUIRED_MESSAGE + "email")
+        @Pattern(regexp = REGEX_NO_BOOLEAN_AND_NUMBER, message = NO_NUMBER_AND_BOOLEAN + "email")
         @Pattern(
                 regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
                 message = INVALID_EMAIL_MESSAGE
